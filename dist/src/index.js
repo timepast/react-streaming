868e514a7d29899a572bc5613233ae2f6d96a128
david <darknight_time@163.com>
2025-01-07 17:31:56 +0800

    feat: add gitignore

--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { hydrateRoot } from "react-dom/client";
-import App from "./App.js";
-import "./index.css";
-import { jsx as _jsx } from "react/jsx-runtime";
-var rootElement = document.getElementById("root");
-hydrateRoot(rootElement, /*#__PURE__*/_jsx(App, {}));
